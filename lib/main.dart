d99ad360b314dd795adf1fd035eba925a5c31dc2
LINDEX171 <lindex171@gmail.com>
2024-11-26 13:13:10 +0000

    column

--- lib/main.dart
+++ lib/main.dart
@@ -28,24 +28,37 @@ class _HomePageState extends State<HomePage> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      appBar: AppBar(title: Center(child: Text("appli1")), elevation: 12,),
-      body: Container(
-       // color: Colors.orange,
-        padding: EdgeInsets.all(20),
-         margin: EdgeInsets.all(20),
-          height: 200,
-          width: 400,
-          decoration: BoxDecoration(
-            color: Colors.orange,
-            borderRadius: BorderRadius.circular(20)
-          ),
-          alignment: Alignment.topLeft,
-          child: Text("hi $counter")),
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("appli1")),
+        elevation: 12,
+      ),
+      // body: Container(
+      //  // color: Colors.orange,
+      //   padding: EdgeInsets.all(20),
+      //    margin: EdgeInsets.all(20),
+      //     height: 200,
+      //     width: 400,
+      //     decoration: BoxDecoration(
+      //       color: Colors.orange,
+      //       borderRadius: BorderRadius.circular(20)
+      //     ),
+      //     alignment: Alignment.topLeft,
+      //     child: Text("hi $counter")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("text1"),
+          Text("Text2"),
+          Image.network("https://thumbs.dreamstime.com/z/wan-wide-area-network-technology-connects-your-offices-data-centers-cloud-applications-storage-together-271663816.jpg", height: 200 , width: 200,)
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+
+        onPressed: () {
           setState(() {
-            counter+=1;
+            counter += 1;
           });
         },
         child: Icon(Icons.add),
@@ -53,6 +66,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-
-
