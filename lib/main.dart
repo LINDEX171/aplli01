fd5c30e8c08ea68fec9a987e9af89ad05ec194a2
LINDEX171 <lindex171@gmail.com>
2024-12-02 16:33:12 +0000

    menu drawer

--- lib/main.dart
+++ lib/main.dart
@@ -30,17 +30,53 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Image.network(
+                "https://th.bing.com/th/id/OIP.9SmZ8rqMhIoMSnZ5_zK3LgHaFj?rs=1&pid=ImgDetMain",
+              ),
+            ),
+            ListTile(
+              title: Text("parametre"),
+              leading: Icon(Icons.settings,),
+              subtitle: Text("page parameter"),
+              trailing: Icon(Icons.add),
+              onTap: () {
+
+              },
+            ),
+            ListTile(
+              title: Text("profile"),
+              leading: Icon(Icons.person,),
+              subtitle: Text("page profile"),
+
+            ),
+            ListTile(
+              title: Text("help"),
+              leading: Icon(Icons.help,),
+              subtitle: Text("page help"),
+            ),
+            ListTile(
+              title: Text("quittez"),
+              leading: Icon(Icons.cancel,),
+              subtitle: Text("page cancel"),
+            ),
+
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        leading: Icon(Icons.menu),
+        // leading: Icon(Icons.menu),
         title: Text("myappbar"),
         actions: [
-          IconButton(onPressed: () {
-            
-          }, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {
-
-          }, icon: Icon(Icons.more_vert),)
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          )
         ],
         elevation: 14,
       ),
